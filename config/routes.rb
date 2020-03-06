d6481052a1d6168fc17cd6fb422fa2892a8852aa
bounce <johan@waytogo.se>
2020-03-06 17:01:52 +0100

    routes trying to unfuck

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/v1/auth', skip: [:omniauth_callbacks]
   namespace :api do
-    resources :products, only: [:index], constraints: { format: 'json' }
     namespace :v1, defaults: { format: :json } do
+      resources :products, only: [:index]
     end
   end
 end
