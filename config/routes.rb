2e958d6a5fad9cc6b5f93728102d1911f4d2d207
bounce <johan@waytogo.se>
2020-03-08 14:46:20 +0100

    changed typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth', skip: [:omniauth_callbacks]
   namespace :api do
-     list_of_products
     resources :products, only: [:index]
   end
 end
